828f3b9239b691ee06ea6a0cc05a9d62a93c6fdb
Dmitriy Emelyanov <harnblaze@gmail.com>
2023-03-17 16:40:56 +0300

    add redux devtools

--- src/store/store.js
+++ src/store/store.js
@@ -2,5 +2,8 @@ import taskReducer from "./task";
 import { legacy_createStore } from "redux";
 
 export default function configureStore() {
-  return legacy_createStore(taskReducer);
+  return legacy_createStore(
+    taskReducer,
+    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+  );
 }
